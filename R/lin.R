06a626c4c2ac39f25cfd0e134cd0be89c2f42872
jrs9 <jrs9@williams.edu>
2015-01-29 19:56:12 -0800

    3

--- R/lin.R
+++ R/lin.R
@@ -8,11 +8,17 @@
 lin <- function(varx, vary){
 fit <- lm(vary ~ varx)
 fit$coefficients[[1]] ->> int
-fit$coefficients[[2]] ->> slope
-plot(var1, var2)
-abline(int, slope)
+fit$coefficients[[2]] ->> sl0pe
+lindf <<- cbind(varx, vary, int, sl0pe)
+lindf <<- data.frame(lindf)
+lindf <<- tbl_df(lindf)
 cor(varx, vary) ->> rval
 rrval <<- rval^2
-paste0("The y int is ", int, " and the slope is ", slope, ".", " The R value is ", rval, " and the R^2 value is ", rrval, ".") -> answer
+paste0("The y int is ", int, " and the slope is ", sl0pe, ".", " The R value is ", rval, " and the R^2 value is ", rrval, ".") -> answer
 print(answer)
+ggplot(data = lindf, aes(varx, vary)) + geom_point() +
+  xlab("X (Var 1)") +
+  ylab("Y (Var 2)")+
+  ggtitle("Simple Linear Regression")+
+  geom_abline(intercept= int, slope= sl0pe)
 }
